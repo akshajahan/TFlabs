0472b8290f5cdfbfb910b5b6b52833d24487482f
Shajahan <shajahan@rjpinfotek.com>
2022-10-22 07:30:11 +0000

    21oct rg

---
+++ lab7/rootmodule/production/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.88.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    
+  }
+}
+
+module "ResourceGroup" {
+  source = "../../modules/ResourceGroup"
+  base_name = "tfproduction"
+  location = "West US"
+}
+
+module "StorageAccount" {
+  source = "../../modules/StorageAccount"
+  base_name = "tfPRODUCTION"
+  resource_group_name = module.ResourceGroup.rg_name_out
+  location = "West US"
+}
+
+##ref: https://www.youtube.com/watch?v=0YLPfSLbp9Y
